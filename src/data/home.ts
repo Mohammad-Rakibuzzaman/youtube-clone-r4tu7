30d72334c334f54987320cc233634080ad0bd4c8
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-07-07 20:34:41 +0600

    [add] project added and category done

---
+++ src/data/home.ts
@@ -0,0 +1,21 @@
+export const categories = [
+    "All",
+    "ReactJS",
+    "TypeScript",
+    "Object Oriented Programming",
+    "Programming",
+    "Weight Lifting",
+    "Functional Programming",
+    "Frontend Web Development",
+    "JavaScript",
+    "Hiking",
+    "Next.js",
+    "Travelling", 
+    "Music",
+    "TV series", 
+    "Problem solving",
+    "Dancing", 
+    "Humor", 
+    "horror"
+
+  ]
